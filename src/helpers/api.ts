eba6123b17fa12999c7c9167dc3132372077642d
LukePa <lukeparton99@gmail.com>
2025-06-15 16:53:48 +0100

    start of making hook up to api

---
+++ src/helpers/api.ts
@@ -0,0 +1,12 @@
+import type {IState} from "../interfaces/state";
+
+
+export async function getStateFromApi(): Promise<IState> {
+    
+}
+
+
+
+export function saveStateToApi(state: IState) {
+    
+}
